10c0c033e3458f24724e9d588143cdc9ca930da2
KareeMagdyy <karim67899@gmail.com>
2022-04-06 22:30:31 +0200

    fixing mobile navbar

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 const navOpen = document.querySelector(".nav-icon");
 const navClose = document.querySelector(".close-nav");
 const mainNavLinks = document.querySelector(".main-nav_links");
+const mainNavLis = document.querySelectorAll(".main-nav_links li");
 
 navOpen.addEventListener("click", () => {
   mainNavLinks.style.cssText = "transform: translateY(0);";
 });
 navClose.addEventListener("click", () => {
   mainNavLinks.style.cssText = "transform: translateY(-100%);";
+});
+mainNavLis.forEach((link) => {
+  link.addEventListener("click", () => {
+    mainNavLinks.style.cssText = "transform: translateY(-100%);";
+  });
 });
