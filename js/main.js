cc18bbdc0e3a394c28e9bec296c3ab833b1bd48e
KareeMagdyy <karim67899@gmail.com>
2022-04-07 16:37:27 +0200

    To-Top added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const navOpen = document.querySelector(".nav-icon");
 const navClose = document.querySelector(".close-nav");
 const mainNavLinks = document.querySelector(".main-nav_links");
 const mainNavLis = document.querySelectorAll(".main-nav_links li");
+const toTop = document.querySelector(".to-top");
 
 navOpen.addEventListener("click", () => {
   mainNavLinks.style.cssText = "transform: translateY(0);";
@@ -13,4 +14,15 @@ mainNavLis.forEach((link) => {
   link.addEventListener("click", () => {
     mainNavLinks.style.cssText = "transform: translateY(-100%);";
   });
+});
+
+window.onscroll = () => {
+  if (window.scrollY >= 400) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+};
+toTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0 });
 });
